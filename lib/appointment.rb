e75f02dc708acf3b3f5531a0b7e4903cee594396
Marc Steele <marcsteele2014@gmail.com>
2020-10-27 20:12:18 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,12 @@
+class Appointment
+
+  attr_accessor :date, :doctor, :patient
+
+  @@all = []
+
+  def initialize(date, doctor, patient)
+    @date = date
+    @doctor = doctor
+    @patient = patient
+    @@all << self
+  end
