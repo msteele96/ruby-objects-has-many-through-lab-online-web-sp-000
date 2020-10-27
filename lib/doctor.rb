bded82e7376ad68b68936a3f4da8d6dadada6959
Marc Steele <marcsteele2014@gmail.com>
2020-10-27 20:20:25 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,7 +14,7 @@ class Doctor
   end
 
   def new_appointment(date, patient)
-    app = Appointment.new(date, self, patient)
+    app = Appointment.new(date, patient, self)
   end
 
   def appointments
