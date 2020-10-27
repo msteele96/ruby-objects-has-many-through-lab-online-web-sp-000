d3fb1ef2c80b4ea3f3a1fdf7d5c71f57dff4be0a
Marc Steele <marcsteele2014@gmail.com>
2020-10-27 20:10:42 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,28 @@
+class Patient
+
+  attr_accessor
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date, doctor)
+    
+  end
+
+  def appointments
+    
+  end
+
+  def doctors
+    
+  end
+
+end
